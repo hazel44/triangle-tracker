add79137dd72cd0aa2b3da2a38cf2dc20960af24
Santa Claus <miriamhazel44@gmail.com>
2019-02-17 13:54:38 +0300

    added bootstrap4 link

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var triangleSide1 = (document.getElementById('Hypotenuse').value);
 var triangleSide2 = (document.getElementById('Opposite').value);
 var triangleSide3 = (document.getElementById('Adjacent').value);
 
-if(triangleSide1 === null && triangleSide2 === null && triangleSide3 === null){
-  alert("Input values");
-}
+if(triangleSide1.length === 0 && triangleSide2.length === 0 && triangleSide3.length === 0){
+    alert('Please fill in values')
+  }
 else if(triangleSide1 === triangleSide2 && triangleSide2 === triangleSide3 && triangleSide1 === triangleSide3){
 alert ("This is an equilateral triangle!");
 
